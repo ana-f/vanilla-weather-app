8fd64efc81f276fa0c46fad4b5f0f8a28ad8ad84
ana-f <artenova.mail@gmail.com>
2021-06-10 18:46:36 +0100

    added image folder with icons and icon search, renamed icon class

--- src/app.js
+++ src/app.js
@@ -20,19 +20,25 @@ currentDay.innerHTML = `${day}`;
 // weather search
 
 function displayTemperature(response) {
+  console.log(response.data);
   let currentTemp = document.querySelector(".current-temp");
   let city = document.querySelector(".city");
   let weatherCondition = document.querySelector(".weather-condition");
   let wind = document.querySelector(".wind");
   let humidity = document.querySelector(".humidity");
+  let icon = document.querySelector("#icon");
+
   currentTemp.innerHTML = `${Math.round(response.data.main.temp)} °C`;
   city.innerHTML = response.data.name;
   weatherCondition.innerHTML = response.data.weather[0].description;
   wind.innerHTML = `wind: ${Math.round(response.data.wind.speed)} km/h`;
   humidity.innerHTML = `humidity: ${response.data.main.humidity}%`;
+  icon.setAttribute("src", `images/${response.data.weather[0].icon}.png`);
+  icon.setAttribute("alt", `response.data.weather[0].description`);
 }
 
 let apiKey = "031b9daf6c535f08d9a6bdcd27cd718d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&units=metric&appid=${apiKey}`;
+let city = "Tokyo";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
